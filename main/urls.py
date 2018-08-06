a54b03bb5fabe35846030981e70bea5246986803
Hyuristyle <hyuri.pimentel@gmail.com>
2018-08-06 01:25:09 -0300

    Ported templates to Django, making use of templates. Plus, added namespace to app 'main'.

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,11 @@ from django.conf.urls.static import static
 
 from . import views
 
+app_name = "main"
+
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
+    path("About", views.about, name="about"),
+    path("Tools", views.tools, name="tools"),
+    path("Learning", views.learning, name="learning"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,11 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import PhotoEntry
+#from .models import PhotoEntry
+
+class PhotoEntry:
+	def __init__(self, photo, thumbnail, title, position):
+		self.photo = photo
+		self.thumbnail = thumbnail
+		self.title = title
+		self.position = position
+
+	def __str__(self):
+		return f"{self.position} - {self.title}"
+
+tmp_entries = []
+
+for i in range(1, 34):
+	tmp_entries.append(PhotoEntry(f"photo ({i}).jpg", f"thumbnail ({i}).jpg", f"Photo {i}", i))
 
 # Create your views here.
 def index(request):
 	context = {
-		"photo_entries": PhotoEntry.objects.all()
+		"photo_entries": tmp_entries # PhotoEntry.objects.all()
 	}
 	return render(request, "main/index.html", context)
+
+def about(request):
+	return render(request, "main/about.html")
+
+def tools(request):
+	return render(request, "main/tools.html")
+
+def learning(request):
+	return render(request, "main/learning.html")
